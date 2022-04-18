31f2aba6dc04671e9c13fd339149a8fa5ddc5505
RobertaDolabella <102393992+RobertaDolabella@users.noreply.github.com>
2022-04-18 00:37:41 -0300

    Add files via upload
    Problemas: ainda sem conseguir enviar as mensagens. Progresso: recebendo mensagens, status online, layout

--- java.js
+++ java.js
@@ -1,96 +1,160 @@
 let nameLogin;
-let nomeUsuario;
-let horario;
+let visibilidadeMensagem;
+let textoMensagem;
 let estado;
+let nomeUsuario;
 let promiseNome;
-let chamada =[{name: ""}]
+let chamada = [{ name: "" }]
 let intervalo;
+let listaChat;
+let tamanhoChat;
+let i;
+let cortedaLista=""
+let corpo;
+let mensagem = {
+    from: `${nameLogin}`,
+    to: `${visibilidadeMensagem}`,
+    text: `${textoMensagem}`,
+    type: `${estado}`
+}
 function telaMensagem() {
     document.querySelector(".login").classList.add("off")
+    document.querySelector(".login").querySelector("img").classList.add("off")
+    document.querySelector(".login").querySelector("input").classList.add("off")
+    document.querySelector(".login").querySelector("button").classList.add("off")
     document.querySelector(".principal").classList.remove("off")
+    getMensagens()
+    timerMensagem()
 }
+function timerMensagem() {
+    console.log("chamou o timer")
+    let timerAtualizar = setInterval(getMensagens, 20000)
+}
+function ultimaMensagem() {
+    i = 0
+    while (i < tamanhoChat) {
+        if (cortedaLista == "") {
+            return i = 0
+        }
+        if (listaChat[i] !== cortedaLista) {
+            i++
+        }
+        if (cortedaLista == listaChat[i])
+            return i
+    }
+}
+function minhasMensagens() {
+    console.log("entrou nas MinhasMensagens")
+    ultimaMensagem()
+    while (i < tamanhoChat) {
+        if (listaChat[i].to == "todos" || listaChat[i].to == "Todos" || listaChat[i].from == `${nameLogin}`) {
+            corpo.innerHTML += [
+                `<li>
+                    <div class="texto">
+                        <b>(${listaChat[i].time})   ${listaChat[i].from}</b> para <b>${listaChat[i].to}</b>: ${listaChat[i].text}
+                    </div>
+                </li>
+                `
+            ]
+        }
+        i++
+    }
+    console.log(listaChat[i])
+    cortedaLista = listaChat[i]
+    console.log(cortedaLista)
+    corpo.scrollIntoView()
+}
+function atualizarMensagem(chat) {
+    console.log("sucesso na promiseMensagem")
+    listaChat = chat.data
+    tamanhoChat = listaChat.length
+    corpo = document.querySelector("ul")
+    minhasMensagens()
+    }
+function getMensagens() {
+    console.log("entrou no getMensagens")
+    promiseMensagem = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+    promiseMensagem.then(atualizarMensagem)
+    promiseMensagem.catch(erroMensagem)
+    }
 function sideBar() {
     document.querySelector(".side-bar").classList.remove("off")
     document.querySelector(".principal").classList.add("principal-back")
 }
-function comecar(){
+function comecar() {
     promiseNome = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
     promiseNome.then(listaNome)
     console.log("continuou")
 }
-function listaNome(resposta){
-    nomeUsuario = {"name":`${nameLogin}`}
+function listaNome(resposta) {
+    nomeUsuario = { "name": `${nameLogin}` }
     nameLogin = document.querySelector("input").value
     chamada = resposta.data
     console.log(chamada)
-    promiseRegistrar = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",nomeUsuario)
-        promiseRegistrar.then(nomePostado)
-        promiseRegistrar.catch(erro)
+    promiseRegistrar = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nomeUsuario)
+    promiseRegistrar.then(nomePostado)
+    promiseRegistrar.catch(erroPostado)
 }
-function nomePostado(){
-    console.log("vai entrar no timer")
-    getMensagens()
-    timer()
+function nomePostado() {
+    timerOnline()
+    entrounaSala()
 }
-function erro(falha){
+function erroPostado(falha) {
     let statusCode = falha.response.status
     alert(`erro do tipo ${statusCode}`)
     console.log(`erro do tipo ${statusCode}`)
-    if(statusCode == 400){
-    alert("Usuário já existente, escolha outro nome para o seu usuário")
-    document.querySelector("input").value = ""
+    if (statusCode == 400) {
+        alert("Usuário já existente, escolha outro nome para o seu usuário")
+        document.querySelector("input").value = ""
     }
 }
-function timer(){
+function timerOnline() {
     intervalo = setInterval(online, 4999)
     console.log("entrou no timer")
 }
-function online(){
+function online() {
     console.log("chamou a funcao online")
     promiseOnline = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nomeUsuario)
     promiseOnline.then(verificacao)
     promiseOnline.catch(caiu)
-        function verificacao(usuario){
-            console.log("tudo certo")
-            estado = "entrou na sala..."
-        }
-        function caiu(erro){
-            clearInterval(intervalo)
-            let statusCode = erro.response.status
-            alert(statusCode)
-        }   
+    function verificacao(usuario) {
+        console.log("tudo certo")
+        estado = "entrou na sala..."
+    }
+    function caiu(erro) {
+        clearInterval(intervalo)
+        let statusCode = erro.response.status
+        alert(statusCode)
+    }
 }
-function getMensagens(){    
-    promiseMensagem = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
-    promiseMensagem.then(entrounaSala)
-    promiseMensagem.catch(erroMensagem)
-}
-function entrounaSala(sucesso){
+
+function entrounaSala() {
     console.log("entrounaSala executando")
-    let visibilidadeMensagem = "todos"
-    let textoMensagem = "entrou na sala"
+    visibilidadeMensagem = "todos"
+    textoMensagem = "entrou na sala"
     estado = "status"
     console.log("definiu menagens")
-    let mensagem = [{
-        from: `${nameLogin}`,
-        to: `${visibilidadeMensagem}`,
-        text: `${textoMensagem}`,
-        type: `${estado}`
-    }]
     console.log(mensagem)
-    promiseEntrou = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",mensagem);
+    promiseEntrou = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagem)
     promiseEntrou.then(statusOnline)
     promiseEntrou.catch(erroStatus)
-    function statusOnline(sucesso){
+    function statusOnline(sucesso) {
         console.log("mensagem enviada")
         telaMensagem()
+        timerMensagem()
     }
-    function erroStatus(erro){
+    function erroStatus(erro) {
         statusCode = erro.response.status
         alert(statusCode)
     }
 }
-function erroMensagem(falha){
+function erroMensagem(falha) {
     statusCode = falha.response.status
     console.log(statusCode)
+}
+
+function enviar(){
+    textoMensagem = document.querySelector(".mensagem").value
+    estado = "message"
+    promiseEnviar = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",mensagem)
 }
